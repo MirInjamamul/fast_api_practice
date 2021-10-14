b4d3abcf839d93e8a43ca9f0171f2de2f239f5b3
MirInjamamul <injamul120221@gmail.com>
2021-10-14 19:23:19 +0600

    Path and Query Parameter

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ async def hello_world():
     return {"Hello": "world"}
 
 
+@app.get("/component/{component_id}") # Path Parameter
+async def get_component(component_id: int):
+    return {"component_id": component_id}
+
+@app.get("/component/")
+async def read_component(number:int, text:Optional[str]): # Query Parameter
+    return {"number": number,"message":text}
+
+
