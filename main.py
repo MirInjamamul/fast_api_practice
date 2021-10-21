0138dc9c89bd2372ce97ae44f6be45da8101a09e
MirInjamamul <injamul120221@gmail.com>
2021-10-21 22:12:29 +0600

    Required Query Parameters if you remove the default value

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def read_items(item_id:str,q: str=None, short: bool=False):
 
 # Multiple Path and query Parameters
 @app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id:int , item_id:str, q: str=None, short:bool=False):
+async def read_user_item(user_id:int , item_id:str, q: str, short:bool=False):
     item = {"User id":user_id, "Item id":item_id}
 
     if q:
